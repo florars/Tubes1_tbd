0b33cd7c20d3cc10e3fa547c77f457152c21008f
InTheRain <indrswara@gmail.com>
2024-03-08 20:56:11 +0700

    fix conflict

--- src/run-bots.bat
+++ src/run-bots.bat
@@ -1,6 +1,6 @@
 @echo off
-start cmd /c "python main.py --logic TBD --email=aaaaaambat@gmail.com --name=tbd --password=123456 --team etimo"
-@REM start cmd /c "python main.py --logic Random --email=aabweballk@gmail.com --name=random --password=123456 --team etimo"
-@REM start cmd /c "python main.py --logic botGreedyDiamonds --email=a13924@gmail.com --name=diamonds --password=123456 --team etimo"
-@REM start cmd /c "python main.py --logic botGreedyBase --email=a298@gmail.com --name=base --password=123456 --team etimo"
-@REM start cmd /c "python main.py --logic botGreedyTackle --email=a38394@gmail.com --name=tackle --password=123456 --team etimo"
+start cmd /c "python main.py --logic TBD --email=aaa23@gmail.com --name=tbd --password=123456 --team etimo"
+start cmd /c "python main.py --logic Random --email=aab23@gmail.com --name=random1 --password=123456 --team etimo"
+start cmd /c "python main.py --logic botGreedyDiamonds --email=a123@gmail.com --name=diamonds1 --password=123456 --team etimo"
+start cmd /c "python main.py --logic botGreedyBase --email=a287@gmail.com --name=base1 --password=123456 --team etimo"
+start cmd /c "python main.py --logic botGreedyTackle --email=a390@gmail.com --name=tackle1 --password=123456 --team etimo"
