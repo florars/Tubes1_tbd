9ad8c34d3c17888c895b326a98d236ef9c93e618
InTheRain <indrswara@gmail.com>
2024-03-08 20:44:38 +0700

    update

--- src/run-bots.bat
+++ src/run-bots.bat
@@ -1,6 +1,6 @@
 @echo off
-start cmd /c "python main.py --logic TBD --email=aaa@gmail.com --name=tbd --password=123456 --team etimo"
-start cmd /c "python main.py --logic Random --email=aab@gmail.com --name=random --password=123456 --team etimo"
-start cmd /c "python main.py --logic botGreedyDiamonds --email=a1@gmail.com --name=diamonds --password=123456 --team etimo"
-start cmd /c "python main.py --logic botGreedyBase --email=a2@gmail.com --name=base --password=123456 --team etimo"
-start cmd /c "python main.py --logic botGreedyTackle --email=a3@gmail.com --name=tackle --password=123456 --team etimo"
+start cmd /c "python main.py --logic TBD --email=aaaaaambat@gmail.com --name=tbd --password=123456 --team etimo"
+@REM start cmd /c "python main.py --logic Random --email=aabweballk@gmail.com --name=random --password=123456 --team etimo"
+@REM start cmd /c "python main.py --logic botGreedyDiamonds --email=a13924@gmail.com --name=diamonds --password=123456 --team etimo"
+@REM start cmd /c "python main.py --logic botGreedyBase --email=a298@gmail.com --name=base --password=123456 --team etimo"
+@REM start cmd /c "python main.py --logic botGreedyTackle --email=a38394@gmail.com --name=tackle --password=123456 --team etimo"
